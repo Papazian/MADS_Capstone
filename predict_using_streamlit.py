5b14f03b72cc8216486c839bf060302d1479311c
Papazian <Papazian@users.noreply.github.com>
2026-07-31 22:38:34 -0500

    Created drop down box by state

--- predict_using_streamlit.py
+++ predict_using_streamlit.py
@@ -125,13 +125,15 @@ if (credit_union_question=='False'):
 else:
     credit_union=1
 
-southern_state_question = st.selectbox(
-    label="Are you located in a Southern state?",
-    options=['False', 'True'],
-    index=0
+state_question = st.selectbox(
+    label="Which U.S. state or territory is the complaint being filed?",
+    options=['AE','AK','AL','AR','AZ','CA','CO','CT','DC','DE','FL','GA','HI','IA','ID','IL','IN','KS','KY','LA','MA','MD','ME','MI','MN','MO','MS','MT','NC','ND','NE','NH','NJ','NV','NY','OH','OK','OR','PA','PR','RI','SC','TN','TX','UT','VA','WA','WI','WV','NM','SD','WY','VI','VT','AA','AP','GU','AS','MP','PW'],
+    index=24
 )
 
-if (southern_state_question=='False'):
+Southern_States = ['AL', 'AR', 'DE', 'FL', 'GA', 'KY', 'LA', 'MD', 'MS', 'NC', 'OK', 'SC', 'TN', 'TX', 'VA', 'WV']
+
+if (state_question not in Southern_States):
     southern_state=0
 else:
     southern_state=1
